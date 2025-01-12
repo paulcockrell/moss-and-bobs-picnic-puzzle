7df5ee3a94c7f9d5c0991b42e79a7146642b8e02
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-01-12 16:22:35 +0000

    Add map

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,33 @@ import { displayDialogue, makeBackground, setCamScale } from "./utils";
 import { SCALE_FACTOR } from "./contants";
 
 const k = kaplay({
-  width: 1280,
-  height: 720,
-  letterbox: true,
   global: false,
-  scale: 2,
 });
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 
-k.loadSprite("spritesheet", "./spritesheet.png", {
-  sliceX: 39,
-  sliceY: 31,
+k.loadSprite("spritesheet", "./tilemap_packed.png", {
+  sliceX: 16,
+  sliceY: 16,
   anims: {
-    "idle-down": 936,
-    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
-    "idle-side": 975,
-    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
-    "idle-up": 1014,
-    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+    "idle-down": 114,
+    "walk-down": { from: 114, to: 114, loop: true, speed: 8 },
+    "idle-side": 114,
+    "walk-side": { from: 114, to: 114, loop: true, speed: 8 },
+    "idle-up": 114,
+    "walk-up": { from: 114, to: 114, loop: true, speed: 8 },
   },
 });
-
-k.loadSprite("map", "./map.png");
 
 k.setBackground(k.Color.fromHex("#311047"));
 
 k.scene("start", async (): Promise<void> => {
   makeBackground(k);
 
-  const mapData = await (await fetch("./map.json")).json();
+  const mapData = await (await fetch("./level1.json")).json();
   const layers = mapData.layers;
 
-  const map = k.add([k.sprite("map"), k.pos(0), k.scale(SCALE_FACTOR)]);
+  const map = k.add([k.sprite("spritesheet"), k.pos(0), k.scale(SCALE_FACTOR)]);
 
   const player = k.make([
     k.sprite("spritesheet", { anim: "walk-down" }),
@@ -106,4 +100,3 @@ k.scene("start", async (): Promise<void> => {
 });
 
 k.go("start");
-
